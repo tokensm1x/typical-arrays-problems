2591586c13ddc601a58f56e2e95843948bce829c
Misha Pateev <tokensmix@gmail.com>
2020-09-28 19:43:11 +0300

    feat: TypicalArrayProblems: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort((a, b) => a - b)[0];
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort((a, b) => b - a)[0];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let summ = array.reduce((sum, element) => sum + element);
+        return summ / array.length;
+    }
 }
